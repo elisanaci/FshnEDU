eecd6cde5174107f38128c5df51d116985afc508
Ervis Trupja <ervis.trupja@projekt202.com>
2023-04-24 10:44:46 +0200

    Add project files.

---
+++ ConsoleApp.Test/Program.cs
@@ -0,0 +1,41 @@
+﻿
+string fullName = "Student Fullname";
+Console.WriteLine($"Original value = {fullName}");
+
+fullName = "Student Fullname 2";
+Console.WriteLine($"New value = {fullName}");
+
+const string middleName = "MiddleName";
+Console.WriteLine($"Original value = {middleName}");
+middleName = "MiddleName 2";
+Console.WriteLine($"New value = {middleName}");
+
+
+//DateTime now = DateTime.Now;
+//Console.WriteLine(now);
+//DateTime nowUtc = DateTime.UtcNow;
+//Console.WriteLine(nowUtc);
+
+//var age = "1";
+//var address = "Student Address";
+//var phoneNumber = 2345;
+
+
+//int age = 30;
+//string address = "Student Address";
+//string phoneNumber = "Student Phone Number";
+//string email = "Student Email";
+//string course = "Student Course";
+//string university = "Student University";
+
+//string faculty = "Student Faculty";
+
+//string specialty = "Student Specialty";
+
+//string studentId = "Student ID";
+
+//string universityId = "Student University ID";
+
+
+
+
