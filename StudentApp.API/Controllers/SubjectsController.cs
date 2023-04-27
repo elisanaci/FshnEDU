66d054b39465e5679b06e57dfcbf76c9791f2e11
Ervis Trupja <ervis.trupja@projekt202.com>
2023-04-27 12:56:32 +0200

    updated subjects controller

--- StudentApp.API/Controllers/SubjectsController.cs
+++ StudentApp.API/Controllers/SubjectsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StudentApp.API.Data;
 using StudentApp.API.Data.DTOs.Subject;
+using StudentApp.API.Data.Models;
 
 namespace StudentApp.API.Controllers
 {
@@ -8,29 +10,49 @@ namespace StudentApp.API.Controllers
     [ApiController]
     public class SubjectsController : ControllerBase
     {
+        private AppDbContext _appDbContext;
+        public SubjectsController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
         //Krijo nje API endpoint per te marre te dhenat nga DB
         [HttpGet("GetAllSubjects")]
         public IActionResult GetAllSubjects()
         {
-            //Marrim te gjithe lendet/subjects nga databasa
+            var allSubjects = _appDbContext.Subjects.ToList();
 
-            //return NotFound();
-            //return BadRequest();
-
-            return Ok();
+            return Ok(allSubjects);
         }
 
         //Krijo nje API endpoint per te marre te dhenat nga DB
         [HttpGet("GetSubjectById/{id}")]
         public IActionResult GetSubjectById(int id)
         {
-            return Ok($"Subjecti me id = {id} u kthye me sukses!");
+            var subject = _appDbContext.Subjects.FirstOrDefault(x => x.Id == id);
+            
+            if(subject == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(subject);
         }
         
         [HttpPost("AddSubject")]
         public IActionResult AddSubject([FromBody] PostSubjectDTO payload)
         {
+            Subject newSubject = new Subject()
+            {
+                Name = payload.Name,
+                Code = payload.Code,
+                DateCreated = DateTime.UtcNow
+            };
 
+            _appDbContext.Subjects.Add(newSubject);
+            _appDbContext.SaveChanges();
+            
             return Ok("Subjecti u krijua me sukses!");
         }
 
@@ -38,17 +60,32 @@ namespace StudentApp.API.Controllers
         public IActionResult UpdateSubject([FromBody]PutSubjectDTO payload, int id)
         {
             //1. Duke perdour ID marrim te dhenat nga databaza
+            var subject = _appDbContext.Subjects.FirstOrDefault(x => x.Id == id);
 
             //2. Perditesojme Subjectin e DB me te dhenat e payload-it
+            if (subject == null)
+                return NotFound();
+
+            subject.Name = payload.Name;
+            subject.Code = payload.Code;
 
             //3. Ruhen te dhenat ne database
+            _appDbContext.Subjects.Update(subject);
+            _appDbContext.SaveChanges();
 
-            return Ok("Subjecti u modifikua me sukses!");
+            return Ok();
         }
 
         [HttpDelete("DeleteSubject/{id}")]
         public IActionResult DeleteSubject(int id)
         {
+            var subject = _appDbContext.Subjects.FirstOrDefault(x => x.Id == id);
+
+            if (subject == null)
+                return NotFound();
+
+            _appDbContext.Subjects.Remove(subject);
+            _appDbContext.SaveChanges();
 
             return Ok($"Subjecti me id = {id} u fshi me sukses!");
         }
