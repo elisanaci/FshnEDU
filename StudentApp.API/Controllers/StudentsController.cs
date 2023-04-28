89091dec29efabc1800446f52a8cb71b7e32153e
Ervis Trupja <ervis.trupja@projekt202.com>
2023-04-28 10:24:10 +0200

    Added DbContext to StudentsController

--- StudentApp.API/Controllers/StudentsController.cs
+++ StudentApp.API/Controllers/StudentsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentApp.API.Data;
 using StudentApp.API.Data.DTOs.Student;
+using StudentApp.API.Data.Models;
 
 namespace StudentApp.API.Controllers
 {
@@ -7,30 +9,45 @@ namespace StudentApp.API.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
-        //Koment
+        private AppDbContext _appDbContext;
+        public StudentsController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
 
         //Krijo nje API endpoint per te marre te dhenat nga DB
         [HttpGet("GetAllStudents")]
         public IActionResult GetAllStudents()
         {
-            //Marrim te gjithe studentet nga databasa
-
-            //return NotFound();
-            //return BadRequest();
-
-            return Ok();
+            var allStudents = _appDbContext.Students.ToList();
+            return Ok(allStudents);
         }
 
         //Krijo nje API endpoint per te marre te dhenat nga DB
         [HttpGet("GetStudentById/{id}")]
         public IActionResult GetStudentById(int id)
         {
+            var student = _appDbContext.Students.FirstOrDefault(x => x.Id == id);
             return Ok($"Studenti me id = {id} u kthye me sukses!");
         }
 
         [HttpPost("AddStudent")]
         public IActionResult AddStudent([FromBody] PostStudentDTO payload)
         {
+            //1. Krijo nje objekt Student me te dhenat e marra nga payload
+            Student newStudent = new Student()
+            {
+                StudentNumber = payload.StudentNumber,
+                FirstName = payload.FirstName,
+                LastName = payload.LastName,
+                DOB = DateTime.UtcNow.AddYears(-20),
+                GPA = payload.GPA,
+                DateCreated = DateTime.UtcNow
+            };
+
+            _appDbContext.Students.Add(newStudent);
+            _appDbContext.SaveChanges();
 
             return Ok("Studenti u krijua me sukses!");
         }
@@ -39,10 +56,20 @@ namespace StudentApp.API.Controllers
         public IActionResult UpdateStudent([FromBody]PutStudentDTO payload, int id)
         {
             //1. Duke perdour ID marrim te dhenat nga databaza
+            var student = _appDbContext.Students.FirstOrDefault(x => x.Id == id);
+            if (student == null)
+                return NotFound();
 
             //2. Perditesojme Studentin e DB me te dhenat e payload-it
+            student.StudentNumber = payload.StudentNumber;
+            student.FirstName = payload.FirstName;
+            student.LastName = payload.LastName;
+            student.DOB = DateTime.UtcNow.AddYears(-20);
+            student.GPA = payload.GPA;
 
             //3. Ruhen te dhenat ne database
+            _appDbContext.Students.Update(student);
+            _appDbContext.SaveChanges();
 
             return Ok("Studenti u modifikua me sukses!");
         }
@@ -50,6 +77,14 @@ namespace StudentApp.API.Controllers
         [HttpDelete("DeleteStudent/{id}")]
         public IActionResult DeleteStudent(int id)
         {
+            //1. Duke perdour ID marrim te dhenat nga databaza
+            var student = _appDbContext.Students.FirstOrDefault(x => x.Id == id);
+            if (student == null)
+                return NotFound();
+
+            //2. Fshijme studentin nga DB
+            _appDbContext.Students.Remove(student);
+            _appDbContext.SaveChanges();
 
             return Ok($"Studenti me id = {id} u fshi me sukses!");
         }
